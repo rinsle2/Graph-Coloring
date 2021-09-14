a043edf2218c54a59be4ec64db51165bfb747417
Rai <ryaninsley2@gmail.com>
2021-09-14 10:43:37 -0500

    some behavior changed, condition updated

--- src/Graph.java
+++ src/Graph.java
@@ -6,12 +6,17 @@ import java.util.LinkedList;
 
 
 public class Graph<T> {
+    //Fields
     private final Map<T, List<T>> map = new HashMap<>();
     private int nodeCount = 0;
     private ArrayList<Boolean> checks;
-
+    //Populate Boolean Array
+    private void populate(int numC) {
+        for (int i=0;i<=numC;i++) {
+            checks.add(false);
+        }
+    }
     Graph() {
-
     }
     public void addVertex(T n) {
         map.put(n, new LinkedList<>());
@@ -28,10 +33,14 @@ public class Graph<T> {
         map.get(s).add(d);
         map.get(d).add(s);
     }
-
-    private void checkMatrix(LinkedList<T> l, int colors, int nC) {
+    /*
+    *
+    * Checks the graph for proper coloring rules (checking for false cases later)
+    *
+    */
+    private void checkMatrix(LinkedList<T> l, int colors) {
         int len = l.size();
-        if(len == nC - 1 && nC > colors) {
+        if(len == nodeCount - 1 && nodeCount > colors && len >= colors) {
             for (int i=0;i<checks.size();i++) {
                 if (!checks.get(i)) {
                     checks.set(i, true);
@@ -39,21 +48,16 @@ public class Graph<T> {
             }
         }
     }
-    private void populate(int numC) {
-        for (int i=0;i<=numC;i++) {
-            checks.add(false);
-        }
-    }
-    private boolean allTrue(int nC) {
+    private boolean allTrue(int numC) {
         int fullCount = 0;
         for (Boolean b: checks) {
             if(b) fullCount++;
-            if(fullCount >= nC) return true;
+            if(fullCount >= numC) return true;
         }
         return false;
     }
     public void graphColoring(int numColors) {
-        map.forEach((k, v) -> checkMatrix(((LinkedList<T>) v), numColors, nodeCount));
+        map.forEach((k, v) -> checkMatrix(((LinkedList<T>) v), numColors));
         checks = new ArrayList<>();
         populate(numColors);
         System.out.print("Graph is ");
@@ -62,8 +66,4 @@ public class Graph<T> {
         }
         System.out.println("fully colored with " + numColors + " colors.");
     }
-
-
-
-
 }
