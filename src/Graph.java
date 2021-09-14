089e817c92977013bd44594d7d4d3485716ea7bc
Rai <ryaninsley2@gmail.com>
2021-09-14 09:14:18 -0500

    Changed Name of File

---
+++ src/Graph.java
@@ -0,0 +1,69 @@
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class Graph<T> {
+    private final Map<T, List<T>> map = new HashMap<>();
+    private int nodeCount = 0;
+    private ArrayList<Boolean> checks;
+
+    Graph() {
+
+    }
+    public void addVertex(T n) {
+        map.put(n, new LinkedList<>());
+        nodeCount++;
+    }
+
+    public void addEdge(T s, T d) {
+        if(!map.containsKey(s)) {
+            addVertex(s);
+        }
+        if(!map.containsKey(d)) {
+            addVertex(d);
+        }
+        map.get(s).add(d);
+        map.get(d).add(s);
+    }
+
+    private void checkMatrix(LinkedList<T> l, int colors, int nC) {
+        int len = l.size();
+        if(len == nC - 1 && nC > colors) {
+            for (int i=0;i<checks.size();i++) {
+                if (!checks.get(i)) {
+                    checks.set(i, true);
+                }
+            }
+        }
+    }
+    private void populate(int numC) {
+        for (int i=0;i<=numC;i++) {
+            checks.add(false);
+        }
+    }
+    private boolean allTrue(int nC) {
+        int fullCount = 0;
+        for (Boolean b: checks) {
+            if(b) fullCount++;
+            if(fullCount >= nC) return true;
+        }
+        return false;
+    }
+    public void graphColoring(int numColors) {
+        map.forEach((k, v) -> checkMatrix(((LinkedList<T>) v), numColors, nodeCount));
+        checks = new ArrayList<>();
+        populate(numColors);
+        System.out.print("Graph is ");
+        if(allTrue(numColors)) {
+            System.out.print("not");
+        }
+        System.out.println("fully colored with " + numColors + " colors.");
+    }
+
+
+
+
+}
