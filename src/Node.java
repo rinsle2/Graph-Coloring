8304e83780df7efdc6a4c80a69ebb60ce233f684
Rai <ryaninsley2@gmail.com>
2021-09-14 09:08:30 -0500

    Faster Imports

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,9 @@
-import java.util.*;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 public class Node<T> {
     private Map<T, List<T>> map = new HashMap<>();
