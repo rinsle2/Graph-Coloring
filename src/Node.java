4d52b5a09d812131138ba5a340b095b5060dc122
Rai <ryaninsley2@gmail.com>
2021-09-14 08:59:07 -0500

    Added Main and Working Class, Test Cases next.

---
+++ src/Node.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Node<T> {
+    private Map<T, List<T>> map = new HashMap<>();
+    private int nodeCount = 0;
+    private ArrayList<Boolean> checks;
+
+    public void addVertex(T n) {
+        map.put(n, new LinkedList<>());
+        nodeCount++;
+    }
+
+    public void addEdge(T s, T d, boolean bidirectional) {
+        if(!map.containsKey(s)) {
+            addVertex(s);
+        }
+        if(!map.containsKey(d)) {
+            addVertex(d);
+        }
+        map.get(s).add(d);
+        map.get(d).add(s);
+    }
+
+    private void checkMatricies(List l, int colors, int nC) {
+        int len = l.size();
+        if(len == nC - 1 && nC > colors) {
+            for (int i=0;i<checks.size();i++) {
+                if (!checks.get(i)) {
+                    checks.set(i, true);
+                }
+            }
+        }
+    }
+    private void populate(int numC) {
+        for (int i=0;i<=numC;i++) {
+            checks.add(false);
+        }
+    }
+    private boolean allTrue(int nC) {
+        int fullCount = 0;
+        for (Boolean b: checks) {
+            if(b) fullCount++;
+            if(fullCount >= nC) return true;
+        }
+        return false;
+    }
+    public void graphColoring(int numColors) {
+        map.forEach((k, v) -> checkMatricies(v, numColors, nodeCount));
+        checks = new ArrayList<>();
+        populate(numColors);
+        System.out.print("Graph is ");
+        if(allTrue(numColors)) {
+            System.out.print("not");
+        }
+        System.out.println("colorable with " + numColors + " colors.");
+    }
+
+
+
+
+}
