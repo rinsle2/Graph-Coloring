f9344042759f0a227770fd32b0a5fda47d47d0b2
Rai <ryaninsley2@gmail.com>
2021-09-15 19:51:51 -0500

    Cleaned up driver

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,43 @@
 public class Main {
 
-    public static void main(String[] args) {
-        Graph<Integer> g = new Graph<>();
-        for(int i=1;i<5;i++)
+    private void initialVertices(Graph<Integer> g, Integer verts) {
+        int i = 1;
+        do {
             g.addVertex(i);
+            i++;
+        } while(i<verts);
+    }
+    private void  initializeSquareGraph(Graph<Integer> g) {
             g.addEdge(1, 2);
             g.addEdge(2, 3);
             g.addEdge(3, 4);
             g.addEdge(4, 1);
             g.addEdge(2, 4);
             g.addEdge(1, 3);
-//        g.addEdge(1, 2);
-//        g.addEdge(1, 5);
-//        g.addEdge(1, 6);
-//        g.addEdge(2, 7);
-//        g.addEdge(2, 3);
-//        g.addEdge(3, 8);
-//        g.addEdge(3, 4);
-//        g.addEdge(4, 9);
-//        g.addEdge(4, 5);
-//        g.addEdge(5, 10);
-//        g.addEdge(6, 8);
-//        g.addEdge(6, 9);
-//        g.addEdge(8, 10);
-//        g.addEdge(10, 7);
-//        g.addEdge(7, 9);
+    }
+    private void  initializeTriangleGraph(Graph<Integer> g) {
+        g.addEdge(1, 2);
+        g.addEdge(1, 5);
+        g.addEdge(1, 6);
+        g.addEdge(2, 7);
+        g.addEdge(2, 3);
+        g.addEdge(3, 8);
+        g.addEdge(3, 4);
+        g.addEdge(4, 9);
+        g.addEdge(4, 5);
+        g.addEdge(5, 10);
+        g.addEdge(6, 8);
+        g.addEdge(6, 9);
+        g.addEdge(8, 10);
+        g.addEdge(10, 7);
+        g.addEdge(7, 9);
+    }
+
+    public void main(String[] args) {
+        Graph<Integer> g = new Graph<>();
+        initialVertices(g, 5);
+//      initializeSquareGraph();
+        initializeTriangleGraph(g);
         g.graphColoring(3);
     }
 }
